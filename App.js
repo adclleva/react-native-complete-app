4e1c28040c5dd263174aee52732e7ca144fb41ce
Arvin Lleva <arvin.lleva@yahoo.com>
2020-01-30 19:46:21 -0800

    Getting the core components and the basic styles

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // style expects a JS object and we can use numbers for padding 
+    // without specifying the value
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}
+        />
+        <Button title="Press Me"/>
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
